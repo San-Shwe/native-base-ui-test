68f59c573caf8fc2ab03ef38c4dadc0f90005b1d
Alphonzo <sanshwe330@gmail.com>
2022-05-03 16:05:56 +0630

    new ui

--- App.js
+++ App.js
@@ -41,11 +41,23 @@ const Main = ({ navigation }) => {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "blue",
+        backgroundColor: "white",
       }}
     >
+      {/* <Image
+        source={require("./assets/adaptive-icon.png")}
+        style={styles.backgroundImage}
+      /> */}
       <View style={styles.container}>
-        <Text style={styles.mainHeader}>My City Clone</Text>
+        {/* <Image source={require('./assets/adaptive-icon.png')} style= {styles.backgroundImage} resizeMode={Image.resizeMode.sretch}> */}
+        <View>
+          <View>
+            <Text style={styles.mainHeader}>Rakhita</Text>
+          </View>
+          <View>
+            <Text style={styles.subHeader}>Music App</Text>
+          </View>
+        </View>
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
@@ -53,14 +65,14 @@ const Main = ({ navigation }) => {
             onPress={() => navigation.navigate("ImagePickerScreen")}
           >
             <Image
-              source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+              source={require("./assets/adaptive-icon.png")}
               style={styles.logo}
             />
           </TouchableOpacity>
 
           <Text style={styles.instructions}>
-            To share a photo from your phone with a friend, just press the
-            button below!
+            Rakhita music app is awesome, you can listen online and offline. You
+            can upload your own song on Rakhita
           </Text>
         </View>
 
@@ -100,50 +112,6 @@ const Main = ({ navigation }) => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10,
-  },
-  instructions: {
-    color: "#888",
-    fontSize: 18,
-    marginHorizontal: 15,
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "#fff",
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain",
-  },
-  mainHeader: {
-    top: 10,
-    width: "90%",
-    height: 100,
-    padding: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: 30,
-    // fontFamily: "Roboto-Light",
-    color: "#000000",
-  },
-});
 
 const Home = () => {
   return (
@@ -190,7 +158,7 @@ const ImagePickerScreen = () => {
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
-          backgroundColor: "blue",
+          backgroundColor: "white",
         }}
       >
         <View style={styles.container}>
@@ -232,7 +200,7 @@ const ImagePickerScreen = () => {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "blue",
+        backgroundColor: "white",
       }}
     >
       <View style={styles.container}>
@@ -261,5 +229,61 @@ const ImagePickerScreen = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 200,
+    marginBottom: 10,
+    // backgroundColor: "red",
+    transform: [{ rotate: "45deg" }],
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+  mainHeader: {
+    top: 10,
+    width: "100%",
+    height: 100,
+    padding: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 30,
+    // fontFamily: "Roboto-Light",
+    color: "#000000",
+  },
+  subHeader: {
+    width: "100%",
+    textAlign: "center",
+    color: "#000000",
+  },
+  backgroundImage: {
+    flex: 1,
+    alignSelf: "stretch",
+    width: null,
+  },
+});
 
 export default App;
