e20d93babeba45b16115c4c17dd1c92b4d7d1f05
San Shwe <sanshwe330@gmail.com>
2022-05-22 09:17:50 +0630

    add track player

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import { registerRootComponent } from "expo";
-
+// import { AppRegistry, Platform } from "react-native"; // new
 import App from "./App";
+import TrackPlayer from "react-native-track-player";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
+TrackPlayer.registerPlaybackService(() => require("./service"));
+
+// new
+// AppRegistry.registerComponent("X", () => App);
+// if (Platform.OS === "web") {
+//   const rootTag =
+//     document.getElementById("root") || document.getElementById("X");
+//   AppRegistry.runApplication("X", { rootTag });
+// }
